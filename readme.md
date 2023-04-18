f5f3728fc11b53ab1306997e6e36e4cd04ed9ba6
tsantaniony <131182203+tsantaniony@users.noreply.github.com>
2023-04-18 19:03:39 +0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Readme
+
+## Comment jouer
+
+On a un tableau à 2 dimensions, c'est-a-dire une matrice carrée dont
+l'index [0][0] est la position tout en bas à gauche
+ l'index [0][0] est la position tout en bas à gauche
+- Une erreur doit se produire si le deplacement est impossible
+
+
---
+++ script.js
@@ -0,0 +1,258 @@
+let position = document.querySelectorAll(".position");
+let tour = document.querySelector(".container");
+
+let player = "p1";
+for(var elt of position){
+    var tmp,
+        from;
+
+    elt.addEventListener("click", function(e){
+
+        if(!tour.classList.contains("moving")) {
+
+            if(tour.classList.contains("player1")) {
+
+                if(this.classList.contains("p1")) {
+                    from = this.id;
+
+                    this.classList.remove("p1");
+                    this.classList.add("active-p1");
+                    tmp = this; 
+
+                    tour.classList.add("moving");
+                }
+                else {
+                    alert("Veuillez choisir un pion BLANC");
+                }
+
+            }
+
+            else if(tour.classList.contains("player2")) {
+
+                if(this.classList.contains("p2")) {
+                    from = this.id;
+
+                    console.log(this.id);
+
+                    this.classList.remove("p2");
+                    this.classList.add("active-p2"); 
+                    tmp = this;
+
+                    tour.classList.add("moving");
+                }
+                else {
+                    alert("Veuillez choisir un pion NOIR");
+                }
+
+            }
+
+        }
+        else {
+            
+            if(tour.classList.contains("player1")) {
+
+                if(this != tmp) {
+                    if(!this.classList.contains("p1") && !this.classList.contains("p2") && isValid(from, this.id)) {
+                        moveTo(from, this.id);
+                        this.classList.add("p1");
+                        tmp.classList.remove("active-p1"); 
+
+                        tour.classList.remove("moving");
+                        tour.classList.remove("player1");
+                        tour.classList.add("player2");
+                    }
+                    else {
+                        alert("Mouvement impossible");
+                    }
+
+                    if(gameOver(1)){
+                        alert("BRAVO AUX PIONS BLANCS! Vous avez gagné ");
+                        window.location.reload();
+                    }
+
+                }
+                else {
+                    this.classList.add("p1");
+                    this.classList.remove("active-p1");
+
+                    tour.classList.remove("moving");
+                }
+
+            }
+
+            else if(tour.classList.contains("player2")) {
+
+                if(this != tmp) {
+
+                    if(!this.classList.contains("p1") && !this.classList.contains("p2") && this != tmp && isValid(from, this.id)) {
+                        moveTo(from, this.id);
+
+                        this.classList.add("p2");
+                        
+                        tmp.classList.remove("active-p2"); 
+
+                        tour.classList.remove("moving");
+                        tour.classList.remove("player2");
+                        tour.classList.add("player1");
+                    }
+                    else {
+                        alert("Mouvement impossible");
+                    }
+
+                    if(gameOver(2)){
+                        alert("BRAVO AUX PIONS NOIRS! Vous avez gagné ");
+                        window.location.reload();
+                    }
+
+                }
+                else {
+                    this.classList.add("p1");
+                    this.classList.remove("active-p1");
+
+                    tour.classList.remove("moving");
+                }
+
+            }
+
+        }
+
+    }, false);
+}
+let tab = [
+    [1, 1, 1],
+    [0, 0, 0],
+    [2, 2, 2]
+];
+
+// Deplacement
+function moveTo(id1, id2) {
+    var start = id1.split("-")[1].split("");
+    var end = id2.split("-")[1].split("");
+
+    if(tab[end[0]][end[1]] != 0){
+        console.log("Erreur!!")
+        return;
+    }
+
+    tab[end[0]][end[1]] = tab[start[0]][start[1]];
+    tab[start[0]][start[1]] = 0;
+}
+
+
+function display() {
+    for(var i=0; i<3; i++) {
+        console.log(tab[i][0], " ", tab[i][1], " ", tab[i][2]);
+    }
+}
+
+
+function isValid(id1, id2) {
+    var start = id1.split("-")[1];
+    var end = id2.split("-")[1];
+
+    
+    if(start == "11")
+        return true;
+
+    if(start === "00") {
+        if(end === "01" || end === "10" || end === "11")
+            return true;
+    }
+
+
+    if(start === "02") {
+        if(end === "01" || end === "12" || end === "11")
+            return true;
+    }
+
+    if(start === "20") {
+        if(end === "21" || end === "10" || end === "11")
+            return true;
+    }
+
+    if(start === "22") {
+        if(end === "21" || end === "12" || end === "11")
+            return true;
+    }
+
+    if(start === "01") {
+        if(end === "00" || end === "02" || end === "11")
+            return true;
+    }
+
+    if(start === "10") {
+        if(end === "00" || end === "20" || end === "11")
+            return true;
+    }
+
+    if(start === "21") {
+        if(end === "20" || end === "22" || end === "11")
+            return true;
+    }
+
+    if(start === "12") {
+        if(end === "02" || end === "22" || end === "11")
+            return true;
+    }
+    
+    return false;
+}
+
+//tests mba ahitana izay mandresy=> alignés horizontalement, verticalement, et obliquement
+function isAlignedX(x){
+    var compt = 0;
+
+    for(var i=0;i<3;i++){
+        for(var j=0;j<3;j++){
+            if(tab[i][j] == x){
+                compt++;
+            }
+        }
+        if(compt == 3){
+            return true;
+        }
+        compt = 0;
+    }
+    return false;
+}
+
+function isAlignedY(x){
+    var compt = 0;
+    for(var i=0;i<3;i++){
+        for(var j=0;j<3;j++){
+            if(tab[j][i] == x){
+                compt++;
+            }
+        }
+        if(compt == 3){
+            return true;
+        }
+        compt = 0;
+    }
+    return false;
+}
+
+function isAlignedDiag(x){
+    if(x == tab[0][0] || x == tab[2][2] || x == tab[1][1])
+        if(tab[1][1] == tab[0][0] && tab[1][1] == tab[2][2])
+            return true;
+    
+    if(x == tab[0][2] || x == tab[2][0] || x == tab[1][1])
+        if(tab[1][1] == tab[0][2] && tab[1][1] == tab[2][0])
+            return true;
+
+    return false;
+}
+
+
+let mandresyA = 0;
+let mandresyB = 0;
+
+function gameOver(x){
+    if(isAlignedX(x) || isAlignedY(x) || isAlignedDiag(x))
+        return true;
+
+    return false;
+}
+
+  
